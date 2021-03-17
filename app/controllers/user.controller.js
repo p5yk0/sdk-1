fcbb69b6d828a1e24a2adc7edd04c779bfeea320
Azaa17 <aza-1998@hotmail.com>
2021-03-17 17:13:06 +0500

    skynet file uploading

---
+++ app/controllers/user.controller.js
@@ -0,0 +1,50 @@
+const fs = require('fs');
+const { SkynetClient } = require('@nebulous/skynet');
+
+const client = new SkynetClient("https://sialoop.net/");
+
+exports.uploadIM = async (req, res) => {
+  const file = req.files.file;
+  file.mv('./uploads/' + file.name, async function(err, result) {
+    if(err) { throw err; }
+    try {
+      const skylink = await client.uploadFile('./uploads/' + file.name);
+      res.send(`Upload successful, skylink: ${skylink}`);
+    } catch (err) {
+      res.status(404).send(err);
+    }
+  });
+};
+
+exports.uploadEX = async (req, res) => {
+  const { internalId, name, descripion, media, cryptedMedia } = req.body;
+
+  let modifiedData = {
+    internalId,
+    name,
+    descripion,
+    media: {
+      type: "image/png",
+      url: media,
+      height: 250,
+      width: 250
+    },
+    cryptedMedia: {
+      url: cryptedMedia,
+    }
+  }
+
+  try {
+    fs.writeFileSync('./uploads/test.json', JSON.stringify(modifiedData));
+
+    const skylink = await client.uploadFile('./uploads/test.json');
+
+    console.log(213)
+    console.log(skylink)
+    // res.send(`Upload successful, skylink: ${skylink}`);
+
+  } catch (err) {
+    console.error(err)
+    // res.status(404).send(err);
+  }
+};
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const fileUpload = require('express-fileupload');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(fileUpload());
+
+app.get('/', (req, res) => {
+    res.send('server is running');
+});
+
+require("./app/routes/user.routes")(app);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
