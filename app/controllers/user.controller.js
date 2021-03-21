1991b9e33c6c1b15aef529bd70c2706d971bfb6f
Mickaël <contact@netick.fr>
2021-03-21 15:22:54 +0100

    sign NFT and Send

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,28 +1,44 @@
 const fs = require('fs');
 const { SkynetClient } = require('@nebulous/skynet');
-const { stringToU8a, u8aToHex } = require('@polkadot/util');
-const { signatureVerify, cryptoWaitReady, decodeAddress, mnemonicGenerate, mnemonicToMiniSecret, mnemonicValidate, naclKeypairFromSeed, blake2AsHex } = require('@polkadot/util-crypto');
+const { stringToU8a, u8aToHex, u8aToString } = require('@polkadot/util');
+const { signatureVerify, encodeAddress, cryptoWaitReady, decodeAddress, mnemonicGenerate, mnemonicToMiniSecret, mnemonicValidate, naclEncrypt, naclKeypairFromSeed, blake2AsHex, randomAsU8a, naclSign } = require('@polkadot/util-crypto');
 const { Keyring } = require('@polkadot/keyring');
 const { exit } = require('process');
 const encrypt = require('node-file-encrypt');
 const { ApiPromise, WsProvider } = require('@polkadot/api');
 const ENDPOINT = 'wss://chaos.ternoa.com';
 const { spec } = require('../types')
-
 const client = new SkynetClient();
+
+const isValidSignature = (signedMessage, signature, address) => {
+  const publicKey = decodeAddress(address);
+  const hexPublicKey = u8aToHex(publicKey);
+
+  return signatureVerify(signedMessage, signature, hexPublicKey).isValid;
+};
 
 exports.mnemonicGenerate = async (req, res) => {
 
-const mnemonic = mnemonicGenerate();
-const keyring = new Keyring();
-const pair = keyring.createFromUri(mnemonic);
+  const keyring = new Keyring({ type: 'sr25519' });
+  const mnemonic = mnemonicGenerate();
+  const newAccount = await keyring.addFromUri(mnemonic);
 
 
-  let account = { 
+  let account = {
     mnemonic: mnemonic,
-    addrss:  pair.address
-    
-};
+    address: newAccount.address,
+
+  };
+  
+  /*
+  const isValid = isValidSignature(
+    'manger',
+    '0x800628fa0e3b23d39863c9d9f8cd9fdad64847e007908297fc9f3620c754416f40ef8ca0329b9b19c93a6197311e55a49f4e47fa84b6a95cde536df9e27ab988',
+    '5HosFh3chaShbZwjR9xUTUXaUCNRj9DRo4774JXe83hTimp1'
+  );
+  console.log(isValid)
+  
+  */
 
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(account));
@@ -111,12 +127,9 @@ exports.uploadEX = async (req, res) => {
 
 
 
-exports.signAndSendKey = async (req, res) => {
-
-}
-
 
 exports.createNft = async (req, res) => {
+
   const { nftUrl } = req.body;
 
   async function main() {
@@ -132,20 +145,48 @@ exports.createNft = async (req, res) => {
         offchain_uri: nftUrl,
       })
       .signAndSend(user, async ({ events = [], status }) => {
-        
+
 
         if (status.isFinalized) {
           unsub();
           events.forEach(async ({ event: { data, method, section } }) => {
-              const nftId = data[0].toString();
-              res.send(nftId);
+            const nftId = data[0].toString();
+            res.send(nftId);
 
-              })
-          }
+          })
+        }
 
       })
   }
   main();
- 
+
 
 };
+
+
+exports.signAndSendKey = async (req, res) => {
+
+  const { nftid } = req.body;
+
+  async function main() {
+    
+    await cryptoWaitReady();
+
+    const keyring = new Keyring({ type: 'sr25519' });
+    const user = await keyring.addFromUri(process.env.mnemonic);
+    const message = stringToU8a(nftid);
+    const signature = user.sign(message);
+
+    let result = {
+      nftId:nftid,
+      signature: u8aToHex(signature),
+      address: user.address,
+  
+    };
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(result));
+  
+  }
+  main();
+}
