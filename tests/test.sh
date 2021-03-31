3dbdd962e64488288aaa3c2eb6a2f9a1a5455446
Yannick Bensacq <yannick.bensacq@tagmaster.com>
2021-03-31 22:51:58 +0200

    Reduce time to return a response when creating a new NFT

--- tests/test.sh
+++ tests/test.sh
@@ -2,7 +2,7 @@ INPUT=../private/round1.csv
 OLDIFS=$IFS
 IFS=','
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-while read wallet address Artist nftName  fileSecretName fileName description  
+while read wallet address Artist nftName  fileSecretName fileName description
 do
 #NFT UPLOAD
 imgFile="../private/$fileName"
@@ -43,15 +43,12 @@ id=$(curl --request POST \
 }');
 
 
-exit;
-
-
 #Deploy
 curl --request POST \
   --url http://127.0.0.1:3000/api/sellNFT \
   --header 'Content-Type: application/json' \
   --data '{
-	"nftId":'+$id+'
+	"nftId":'$id'
 }
 	'
 
